6b8841addb1c52e43e8b1d7cc1fd9bf19479b50f
somnathjadhav-apple <88436267+somnathjadhav-apple@users.noreply.github.com>
2021-08-27 12:23:20 +0530

    Initial Commit

---
+++ CemtrexLabs machine test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CemtrexLabs machine test
+//
+//  Created by Somnath on 27/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
